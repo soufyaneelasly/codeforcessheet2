d7c45209a3e90d786991ab32333499d8f68868ba
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-27 00:09:40 +0000

    B

---
+++ B.even/beven.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+using namespace std ;
+
+
+int main(){
+int n ;
+cin >> n;
+
+if(n>=2){
+for(int i=2;i<=n;i++){
+cout<<i<<endl;
+i++;
+}}else{
+cout<<"-1"<<endl;
+}
+
+
+
+
+}
