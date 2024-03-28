2f598dbbc66dbad9ff83b780f2de424a74b6feac
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-28 00:39:47 +0000

    D

---
+++ D.password/Dpassword.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+for(int i=0;i<2;i++){
+int m ;
+cin>>m;
+if(m==1999){
+cout<<"Correct"<<endl;
+break;
+}else{
+cout<<"Wrong"<<endl;
+i--;
+}
+
+}
+
+}
