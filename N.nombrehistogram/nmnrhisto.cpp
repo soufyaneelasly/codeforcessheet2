e691c67c9eb3f9aa7e66c0fcacc6f109b40b1bd9
soufyaneelasly <elasslysoufyane@gmail.com>
2024-03-31 22:38:21 +0000

    N

---
+++ N.nombrehistogram/nmnrhisto.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std ;
+
+
+int main(){
+
+
+
+char c;
+
+int x;
+
+cin>>c>>x;
+
+
+while(x){
+int str;
+cin>>str;
+
+for(int i=0;i<str;i++){
+cout<<c;
+
+}
+cout<<endl;
+x--;
+
+
+}
+
+
+
+
+
+
+
+}
